4aeabcb55f0211a491433a9f9048a0eb0183978b
Ayoub Elhioui <Elhioui001@gmail.com>
2022-02-11 13:43:37 +0100

    done

--- pipex_bonus.c
+++ pipex_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ael-hiou <ael-hiou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/05 14:07:39 by ael-hiou          #+#    #+#             */
-/*   Updated: 2022/02/10 14:16:59 by ael-hiou         ###   ########.fr       */
+/*   Updated: 2022/02/11 13:42:50 by ael-hiou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,18 +22,18 @@ int	ft_strcmp(char *s1, char *s2)
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
 
-void    ft_putstr(char *str)
+void    ft_putstr(char *str, int fd)
 {
     int i;
 
     i = 0;
     while (str[i])
-        write(1, &str[i++], 1);
+        write(fd, &str[i++], 1);
 }
 
 void    error_printing()
 {
-    ft_putstr("Error\n");
+    ft_putstr("Error\n", OUTPUT_FD);
     exit(1);
 }
 
@@ -66,11 +66,10 @@ void    executing_command(char **av, char **env_variables, int arg_position)
 {
     t_arg arg;
 
-    
     arg.splited_command = ft_split(av[arg_position], ' ');
     arg.full_path = get_command_path(env_variables, arg.splited_command[0]);
-    execve(arg.full_path, arg.splited_command, env_variables);
     if (execve(arg.full_path, arg.splited_command, env_variables) == -1){
+        printf("The Command Is : %s\n", arg.splited_command[0]);
         perror("");
         exit(1); 
     }
@@ -105,85 +104,77 @@ void    duplicating(int input, int output)
    dup2(output, 1);  
 }
 
-void    get_lines(char *limiter)
+int get_input_lines(char *limiter)
 {
 	char	*saver;
 	char	*last;
+    int fd;
 
 	saver = NULL;
 	last = get_next_line(0);
+    unlink("/tmp/.temp_file");
+    fd = open("/tmp/.temp_file", O_RDWR | O_CREAT | O_TRUNC, 0777);
 	while (last)
 	{
         if (ft_strcmp(last, limiter) == 0)
-            return ;
+            break ;
+        ft_putstr(last, fd);
+        ft_putstr("\n", fd);
         last = get_next_line(0);
 	}
+    return (fd);
 }
-void    pipe_simulating(t_process_vars *vars, char **av, char **env_variables)
+
+void    pipe_simulating(t_process_vars *vars, char **av, char **env_variables, int index)
 {
-    int i;
-    
-    i = 0;
-    while (i < vars->command_number){
-    pipe(vars->pipes_array[i]);
-    vars->p_ids[i] = fork();
-    if (vars->p_ids[i] == 0){
-        close_pipes(vars->pipes_array, vars->pipes_number, i);
-        if (i == 0){
-            vars->first_file_fd = open(av[FIRST_FILE_ARG], O_RDONLY);
-            if (vars->first_file_fd < 0){
-                perror(av[FIRST_FILE_ARG]);
+    pipe(vars->pipes_array[index]);
+    vars->p_ids[index] = fork();
+    if (vars->p_ids[index] == 0)
+    {
+        close_pipes(vars->pipes_array, vars->pipes_number, index);
+        if (index == 0)
+        {
+            if (vars->first_file_fd < 0)
+            {
+                ft_putstr("no such file or directory\n", OUTPUT_FD);
                 exit(1);
             }
-            duplicating(vars->first_file_fd, vars->pipes_array[i][1]);
+            duplicating(vars->first_file_fd, vars->pipes_array[index][1]);
         }
-        else if (i == vars->command_number - 1)   
-            duplicating(vars->pipes_array[i - 1][0], vars->second_file_fd);
+        else if (index == vars->command_number - 1)   
+            duplicating(vars->pipes_array[index - 1][0], vars->second_file_fd);
         else
-            duplicating(vars->pipes_array[i - 1][0], vars->pipes_array[i][1]);
-        executing_command(av, env_variables, (i + 2));
+            duplicating(vars->pipes_array[index - 1][0], vars->pipes_array[index][1]);
+        executing_command(av, env_variables, (index + 2));
     }
-    i++;
 }
-}
+
 int main(int ac, char **av, char **env_variables)
 {
     t_process_vars vars;
     int i;
 
-    i = 0;
     vars.command_number = ac - 3;
     vars.p_ids = malloc(sizeof(int) * vars.command_number);
     vars.pipes_number = vars.command_number - 1;
     vars.second_file_fd = open(av[ac - 1], O_RDWR | O_CREAT | O_TRUNC, 0777);
-    if (ft_strcmp(av[FIRST_FILE_ARG], "here_doc") == 0){
-        get_lines(av[2]);
+    if (ft_strcmp(av[FIRST_FILE_ARG], "here_doc") == 0)
+    {
+        vars.first_file_fd = get_input_lines(av[2]);
+        vars.pipes_number--;
+        vars.command_number--;
     }
+    else
+        vars.first_file_fd = open(av[FIRST_FILE_ARG], O_RDONLY);
     vars.pipes_array = malloc(sizeof(int *) * vars.pipes_number);
     if (access(av[ac - 1], W_OK) != 0 || ac < 5)
         error_printing();
-    // while (i < vars.command_number){
-    //     pipe(vars.pipes_array[i]);
-    //     vars.p_ids[i] = fork();
-    //     if (vars.p_ids[i] == 0){
-    //         close_pipes(vars.pipes_array, vars.pipes_number, i);
-    //         if (i == 0){
-    //             vars.first_file_fd = open(av[FIRST_FILE_ARG], O_RDONLY);
-    //             if (vars.first_file_fd < 0){
-    //                 perror(av[FIRST_FILE_ARG]);
-    //                 exit(1);
-    //             }
-    //             duplicating(vars.first_file_fd, vars.pipes_array[i][1]);
-    //         }
-    //         else if (i == vars.command_number - 1)   
-    //             duplicating(vars.pipes_array[i - 1][0], vars.second_file_fd);
-    //         else
-    //             duplicating(vars.pipes_array[i - 1][0], vars.pipes_array[i][1]);
-    //         executing_command(av, env_variables, (i + 2));
-    //     }
-    //     i++;
-    // }
-    pipe_simulating(&vars, av, env_variables);
+    i = 0;
+    while (i < vars.command_number)
+    {
+         pipe_simulating(&vars, av, env_variables, i);
+        i++;
+    }
     close_pipes(vars.pipes_array, vars.pipes_number, -1);
     wait_for_childs(vars.p_ids, vars.command_number);
     close(vars.second_file_fd);
